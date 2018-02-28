f00a364ffcbb4cdc557653168b4b252874ddd008
ahardencoopers <ahardencoopers@gmail.com>
2018-02-28 11:29:09 -0600

    Actualizar base de datos para endpoint /lista_manejar_usuarios

--- condos-api/api.rb
+++ condos-api/api.rb
@@ -4,12 +4,17 @@ require './database.rb'
 
 set(:port, 5000)
 
+#Valida un intento de login
+#params: username, password
 post '/validar_login' do
 	#todo crear sesion despues de validar login
 	return Database.validar_login(params[:username], params[:password]).to_json()
 end
 
+#Regresa la lista de todos los usuarios del condominio (inquilinos, propietarios y admins)
+#Solo accesible para admins
+#params: admin_username, nombre_condo
 post '/lista_manejar_usuarios' do
 	#todo validar sesion antes de regresar lista
-	return Database.lista_manejar_usuarios(params[:admin_username], params[:condominio]).to_json()
+	return Database.lista_manejar_usuarios(params[:admin_username], params[:nombre_condo]).to_json()
 end
