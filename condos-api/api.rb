cc7fb32be4bf311dbecb95800109cd2ab80a80d1
ahardencoopers <ahardencoopers@gmail.com>
2018-02-27 20:40:50 -0600

    En database.rb: Agregar endpoint validar_login para validar intentos de login, agregar metodos para obtener datos de login de los diferentes tipos de usuario; en api.rb: descomentar lineas para habilitar endpoint validar_login no dummy

--- condos-api/api.rb
+++ condos-api/api.rb
@@ -1,10 +1,9 @@
 require 'sinatra'
 require 'json'
-#require './database.rb'
+require './database.rb'
 
 set(:port, 5000)
 
 post '/validar_login' do
-	#return Database.validar_login(params[:username], params[:password])
-	return [:status => 'ok'].to_json()
+	return Database.validar_login(params[:username], params[:password]).to_json()
 end
--- condos-api/database.rb
+++ condos-api/database.rb
@@ -1,17 +1,65 @@
 require 'mongo'
 
 module Database
+	
 	def self.validar_login(username, password)
 		Mongo::Logger.logger.level = Logger::FATAL
 		mongo = Mongo::Client.new([Socket.ip_address_list[1].inspect_sockaddr + ':27017'], :database => 'condominios')
+
 		if mongo[:admins].find({:username => username, :password => password}).count() == 1
-			return {:status => 'ok', :data => 'admin'}
+			return {:status => 'ok', :data => 'super_admin'}
 		end
-		usuario = mongo[:usuarios].find({:username => username, :password => password})
-		if usuario.count() != 1
+
+		if mongo[:usuarios].find({:username => username, :password => password}).count() != 1
 			return {:status => 'error', :data => 'Login invalido'}
 		end
-		tipos_de_usuario = mongo[:usuario_tiene_tipos].find({:username => username}).to_a()
+		res = {:status => 'ok', :data => {}}
+
+		if self.es_tipo(username, 'inquilino')
+			res[:data][:inquilino] = self.datos_login_inquilino(username)
+		end
+
+		if self.es_tipo(username, 'propietario')
+			res[:data][:propietario] = self.datos_login_propietario(username)
+		end
+
+		if self.es_tipo(username, 'admin')
+			res[:data][:admin] = self.datos_login_admin(username)
+		end
+
+		return res
+		mongo.close()
+	end
+
+	def self.es_tipo(username, tipo)
+		Mongo::Logger.logger.level = Logger::FATAL
+		mongo = Mongo::Client.new([Socket.ip_address_list[1].inspect_sockaddr + ':27017'], :database => 'condominios')
+		if mongo[:usuario_tiene_tipos].find({:username => username, :tipo => tipo}).count() >= 1
+			return true
+		end
+		return false
+		mongo.close()
+	end
+
+	def self.datos_login_admin(username)
+		Mongo::Logger.logger.level = Logger::FATAL
+		mongo = Mongo::Client.new([Socket.ip_address_list[1].inspect_sockaddr + ':27017'], :database => 'condominios')
+		return mongo[:administra_condominios].find({:username => username}).to_a()
+		mongo.close()
+	end
+
+	def self.datos_login_inquilino(username)
+		Mongo::Logger.logger.level = Logger::FATAL
+		mongo = Mongo::Client.new([Socket.ip_address_list[1].inspect_sockaddr + ':27017'], :database => 'condominios')
+		return mongo[:vive_en].find({:username => username}).to_a()
+		mongo.close()
+	end
+
+	def self.datos_login_propietario(username)
+		Mongo::Logger.logger.level = Logger::FATAL
+		mongo = Mongo::Client.new([Socket.ip_address_list[1].inspect_sockaddr + ':27017'], :database => 'condominios')
+		return mongo[:propietario_de].find({:username => username}).to_a()
 		mongo.close()
 	end
 end
+
